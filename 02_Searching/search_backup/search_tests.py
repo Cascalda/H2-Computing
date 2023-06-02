8782212efc64216c907f3b35fb7375bcb1e78d0a
Grani7e <111849193+Cascalda@users.noreply.github.com>
2023-06-02 15:08:13 +0800

    Initial Commit

---
+++ 02_Searching/search_backup/search_tests.py
@@ -0,0 +1,86 @@
+from search_backup.randlst_gen import nu_rand1d as randlst
+
+# >>> Quicksearching -----------------------------------------------------------------------------------------
+def find(lst, item):
+    return [i for (i, x) in enumerate(lst) if x == item]
+
+# >>> Linear Search ------------------------------------------------------------------------------------------
+from random import choice
+def linear_search_Test(searches):
+    # Make random list and choose a random item from it
+    lst = randlst([20, 10, 100])
+    item = choice(lst)
+    print(f"item: {item}",
+          f"lst: {lst}", 
+          f"sorted: {sorted(lst)}", 
+          sep = "\n", end = "\n\n")
+    
+    # Run through all the linear search functions
+    for name, search in searches.items():
+        
+        # Sort the list if it is ordered linear search
+        if name == "ordered_linear_search":
+            lst.sort()
+            
+        item_at = search(lst, item)
+        ans = find(lst, item)
+        valid = (item_at == ans)
+        print(f">>> {name}: {item_at}", 
+              f"Expected: {ans}", 
+              f"{valid}", 
+              sep = " | ") # ans
+        
+# >>> Binary Search ------------------------------------------------------------------------------------------
+def binary_search_Test(searches):
+    # Make SORTED random list and choose a random item from it
+    lst = sorted(randlst([20, 10, 100]))
+    item = choice(lst)
+    print(f"item: {item}",
+          f"sorted: {lst}", 
+          sep = "\n", end = "\n\n")
+    
+    # Run through all the binary search functions
+    for name, search_function in searches.items():
+        item_at = search_function(lst, item)
+        ans = find(lst, item)
+        valid = (item_at in ans)
+        print(f">>> {name}: {item_at}", 
+              f"Expected: {ans}", 
+              f"{valid}", 
+              sep = " | ") # ans
+        
+# >>> Hash Table Search --------------------------------------------------------------------------------------
+from search_backup.randlst_gen import randstr
+from random import randint
+def hash_table_Test(HT):
+    print(f">>> HashTable:")
+    # Make keys and values
+    r = randint(8, 15)
+    keys = randlst([r, 10, 100])
+    values = randstr(len(keys))
+    print(f"Keys to enter: {keys}",
+          f"values to enter: {values}",
+          f"Key-value pair: {list(zip(keys, values))}",   # display one list with key-value pair
+          sep = "\n", end = "\n\n")
+    
+    # Initiate Hash Table
+    H = HT(len(keys) - 2)
+    
+    # Put the key-value pair into Hash Table
+    print("Put:")
+    for i, (key, value) in enumerate(zip(keys, values)):
+        print(f"Key: {key}",
+              f"Status: {H.put(key, value)}",
+              f"Load factor: {H.load_factor()}", 
+              sep = " | ")
+    print()
+    print(f"Keys entered: {H._keys}",
+          f"values entered: {H._values}", 
+          sep = "\n", end = "\n\n")
+    
+    # Get the values in Hash Table for each corresponding key
+    print("Get:")
+    for key in keys:
+        print(f"Key: {key}",
+              f"({H.get(key)})",
+              sep = " | ")
---
+++ 03_Data Structures/ds_backup/randlst_gen.py
@@ -0,0 +1,47 @@
+dft = [10, 10, 100, 10] # dft = default (range, low, high, row)
+
+## Non-unique random list (1d, 2d)
+from random import randrange
+def nu_rand1d(info = dft[:-1]):
+    r, low, high = info # r: range
+    return [randrange(low, high) for num in range(r)]
+
+def nu_rand2d(info = dft):
+    *info_1d, rows = info
+    return [nu_rand1d(info_1d) for i in range(rows)]
+
+# Unique random list (1d, 2d)
+from random import sample
+def u_rand1d(info = dft[:-1]):
+    r, low, high = info # r: range
+    return sample(range(low, high), r) # unique sample
+
+def u_rand2d(info = dft):
+    *info_1d, rows = info
+    return [u_rand1d(info_1d) for i in range(rows)]
+
+## Test
+# rdict = {"nu_rand1d": nu_rand1d, 
+#          "nu_rand2d": nu_rand2d, 
+#          "u_rand1d": u_rand1d, 
+#          "u_rand2d": u_rand2d,}
+
+# cases = {"1d": [5, 15, 25], "2d": [8, 11, 30, 5]}
+# def rdict_test():
+#     c1d, c2d = cases.values() # get test cases
+#     for name, func in rdict.items(): # loop thru the functions
+#         print(f"{name} -----------------------------------------")
+#         if "1d" in name:
+#             print(f"default: {func()}", 
+#                   f"rand: {func(c1d)}", sep = "\n")
+#         elif "2d" in name:
+#             print(f"default:")
+#             for row in func(): 
+#                 print(row)
+                
+#             print(f"rand:")
+#             for row in func(c2d): 
+#                 print(row)
+#         print()
+        
+# rdict_test()
